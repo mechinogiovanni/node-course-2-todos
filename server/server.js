d374e3d5f0fdb04a5666fc42e6dc553a77eb1e66
Gio4Ever <meschinogiovanni@yahoo.it>
2018-02-06 17:41:17 +0100

    rest post todo

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {User} = require('./models/user.js');
+var {Todo} = require('./models/todo.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) =>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(3001, () => {
+  console.log("Started on port 3000");
+});
