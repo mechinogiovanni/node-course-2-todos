8d60a2e6ef6315a36b83ca53693faff0d02a9730
Gio4Ever <meschinogiovanni@yahoo.it>
2018-02-07 12:21:59 +0100

    adding GET m/todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {User} = require('./models/user.js');
@@ -27,6 +28,26 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+        return res.status(404).send();
+    }
+
+    res.send({todo});
+    
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
 });
 
 app.listen(3000, () => {
