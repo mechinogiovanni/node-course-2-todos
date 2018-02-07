4bdc0ab294a53f256a0f33bfd62a2f4c1c99d93f
Gio4Ever <meschinogiovanni@yahoo.it>
2018-02-07 12:04:49 +0100

    mongose playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,23 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5a7ad84d0194c4240dec77c2';
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo by Id', todo);
+}).catch((e) => console.log(e));
